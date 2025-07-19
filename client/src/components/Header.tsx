6e26a97d8822e75ab4ddfc4da43d503dcd4bfa50
Habeebamoo <habeebamoo08@gmail.com>
2025-07-19 16:01:31 +0100

    updated header font

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header className="flex-between bg-white shadow-sm py-3 px-3 fixed top-0 left-0 right-0 z-50">
       <div className="flex-start p-1">
         <img src={logo} className="h-[30px]" />
-        <h1 className="ml-1 text-black text-xl">FlashQuiz</h1>
+        <h1 className="ml-1 text-black text-xl font-inter">FlashQuiz</h1>
       </div>
       <div>
         <button className="btn-black" onClick={() => navigate("/login")}><FaArrowRightFromBracket /></button>
